eef610a2faa11fbd31ff731c9bb6172c904ea9df
Greg Beecroft <greg.beecroft@taulia.com>
2022-02-16 17:58:16 -0700

    testing something

--- main.tf
+++ main.tf
@@ -125,9 +125,7 @@ resource "aws_transfer_user" "transfer_server_user" {
   user_name      = each.value.username
   role           = join("", aws_iam_role.transfer_server_role.*.arn)
   hdmappings = each.value.username == "taulia" ? {} : {entry = "/", target = "/${aws_s3_bucket.environment[each.value.env].id}/$${Transfer:UserName}"}
-  home_directory_mappings {
-    hdmappings
-  }
+  home_directory_mappings = hdmappings
   hdtype = each.value.username == "taulia" ? "PATH" : "LOGICAL"
   home_directory_type = hdtype
   tags           = module.labels.tags
