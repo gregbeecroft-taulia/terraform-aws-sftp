69e40952d3a69929ff4cbcad325dcb17ebb5211d
Greg Beecroft <greg.beecroft@taulia.com>
2022-02-22 19:04:59 -0700

    Put in var. here

--- main.tf
+++ main.tf
@@ -161,7 +161,7 @@ resource "aws_transfer_user" "transfer_server_user_mulesoft" {
 # Module      : AWS TRANSFER SSH KEY
 # Description : Provides a AWS Transfer User SSH Key resource.
 resource "aws_transfer_ssh_key" "transfer_server_ssh_key" {
-  count = length(local.list_of_pubkeys)
+  count = length(var.list_of_pubkeys)
 
   server_id = var.endpoint_type == "VPC" ? join("", aws_transfer_server.transfer_server_vpc.*.id) : join("", aws_transfer_server.transfer_server.*.id)
   user_name  = var.list_of_users[count.index]
