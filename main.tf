48005dd419a3d58f51e10110681486c11cd7a3e0
Greg Beecroft <greg.beecroft@taulia.com>
2022-02-22 19:02:52 -0700

    Remove the local vars here

--- main.tf
+++ main.tf
@@ -20,8 +20,6 @@ module "labels" {
 locals {
   fulluserlist = var.fulluserlist
   pubkeylist = var.pubkeylist
-  list_of_users = var.list_of_users
-  list_of_pubkeys = var.list_of_pubkeys
 }
 
 
@@ -163,10 +161,10 @@ resource "aws_transfer_user" "transfer_server_user_mulesoft" {
 # Module      : AWS TRANSFER SSH KEY
 # Description : Provides a AWS Transfer User SSH Key resource.
 resource "aws_transfer_ssh_key" "transfer_server_ssh_key" {
-  count = local.list_of_pubkeys
+  count = length(local.list_of_pubkeys)
 
   server_id = var.endpoint_type == "VPC" ? join("", aws_transfer_server.transfer_server_vpc.*.id) : join("", aws_transfer_server.transfer_server.*.id)
-  user_name  = local.list_of_usernames[count.index]
-  body       = local.list_of_pubkeys[count.index]
+  user_name  = var.list_of_users[count.index]
+  body       = var.list_of_pubkeys[count.index]
   depends_on = [ aws_transfer_user.transfer_server_user, aws_transfer_user.transfer_server_user_mulesoft ]
 }
